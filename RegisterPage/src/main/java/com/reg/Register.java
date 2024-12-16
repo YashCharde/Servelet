cc679e69d2c654bdc52e03a0ec3d3378c2eeea6f
Yash Charde <156565536+YashCharde@users.noreply.github.com>
2024-12-16 23:37:34 +0530

    Update Register.java

--- RegisterPage/src/main/java/com/reg/Register.java
+++ RegisterPage/src/main/java/com/reg/Register.java
@@ -44,29 +44,33 @@ public class Register extends HttpServlet {
        System.out.println("Country: " + country);
        
        PrintWriter out = resp.getWriter();
-       
-       out.println("<html>");
+     
+      // Create the response page 
+        out.println("<html>");
        out.println("<head><title>Registration Success</title></head>");
        out.println("<body>");
        out.println("<h1>Registration Successful!</h1>");
-       out.println("<p>Thank you for registering, " + fName + " " + lName + "</p>");
-       out.println("<h3>Registration Details:</h3>");
-       out.println("</html>");
-       out.println(fName);
-       out.println(lName);
-       out.println(email);
-       out.println( password);
-       out.println(mobno);
-       out.println(gender);
-       out.println(dob);
-       out.println(age);
-       out.println(degree);
-       out.println(passing_year);
-       out.println(address);
-       out.println(city);
-       out.println(country);
+       out.println("<p>Thank you for registering, " + fName + " " + lName + "!</p>");
+       out.println("<h3>Your Registration Details:</h3>");
        
-       
-	}
+       // Display the details in a more structured manner
+       out.println("<table border='1' cellpadding='5'>");
+       out.println("<tr><td><strong>First Name</strong></td><td>" + fName + "</td></tr>");
+       out.println("<tr><td><strong>Last Name</strong></td><td>" + lName + "</td></tr>");
+       out.println("<tr><td><strong>Email</strong></td><td>" + email + "</td></tr>");
+       out.println("<tr><td><strong>Password</strong></td><td>" + password + "</td></tr>");
+       out.println("<tr><td><strong>Mobile Number</strong></td><td>" + mobno + "</td></tr>");
+       out.println("<tr><td><strong>Gender</strong></td><td>" + gender + "</td></tr>");
+       out.println("<tr><td><strong>Date of Birth</strong></td><td>" + dob + "</td></tr>");
+       out.println("<tr><td><strong>Age</strong></td><td>" + age + "</td></tr>");
+       out.println("<tr><td><strong>Degree</strong></td><td>" + degree + "</td></tr>");
+       out.println("<tr><td><strong>Passing Year</strong></td><td>" + passing_year + "</td></tr>");
+       out.println("<tr><td><strong>Address</strong></td><td>" + address + "</td></tr>");
+       out.println("<tr><td><strong>City</strong></td><td>" + city + "</td></tr>");
+       out.println("<tr><td><strong>Country</strong></td><td>" + country + "</td></tr>");
+       out.println("</table>");
 
+       out.println("</body>");
+       out.println("</html>");
+   }
 }
